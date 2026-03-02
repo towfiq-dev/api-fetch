ad9e8411306bfcdef50c24f82f486d7e9f071593
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-02 11:53:58 +0600

    Auto-update on Mon, Mar  2, 2026 11:53:58 AM

---
+++ json1.js
@@ -0,0 +1,16 @@
+const url = 'http://jsonplaceholder.typicode.com/users'
+const loadData = ()=>{
+  fetch(url)
+  .then(response=> response.json())
+  .then(data=> displayData(data))
+}
+
+const displayData =(data)=>{
+  const section = document.getElementById('section-id')
+  data.map(user =>{
+    const p = document.createElement('p')
+    p.innerText = user.phone
+    section.appendChild(p)
+  })
+  
+}
