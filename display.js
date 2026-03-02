62f9a6edbc7a4042d2e8f7c6edfd93b5031f884f
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-03-02 16:12:45 +0600

    Auto-update on Mon, Mar  2, 2026  4:12:45 PM

--- display.js
+++ display.js
@@ -1,13 +1,13 @@
-const url = 'https://jsonplaceholder.typicode.com/posts'
+const url = 'https://jsonplaceholder.typicode.com/todos'
 const loadPost = ()=>{
   fetch(url)
   .then(response => response.json())
   .then(data => displayPost(data))
 }
-// body: "quia et suscipit suscipit recusandae consequuntur expedita et cum reprehenderit molestiae ut ut quas totam nostrum rerum est autem sunt rem eveniet architecto"
-// id: 1
-// title: "sunt aut facere repellat provident occaecati excepturi optio reprehenderit"
-// userId: 1
+// "userId": 1,
+// "id": 1,
+// "title": "delectus aut autem",
+// "completed": false
 
 const displayPost=(posts)=>{
   const attach = document.getElementById('container')
@@ -20,10 +20,10 @@ const displayPost=(posts)=>{
     createElement.style.margin = '20px'
     createElement.style.borderRadius = '15px'
     createElement.innerHTML = `
-    <h1>${post.title}</h1>
     <h3>${post.id}</h3>
     <h3>${post.userId}</h3>
-    <p>${post.body}</p>
+    <p>${post.title}</p>
+    <p>${post.completed == true? '<i class="fa-solid fa-calendar-check"></i>' : '<i class="fa-regular fa-calendar-check"></i>'}</p>
     `
     attach.appendChild(createElement)
   });
